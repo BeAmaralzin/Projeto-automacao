1886c6c49eb998bc5c1f63fa9b34ddd967ffe020
BeAmaralzin <bamaralzin@gmail.com>
2026-01-19 18:10:08 -0300

    espera do carregamento do botão

--- arquivo.py
+++ arquivo.py
@@ -1,10 +1,32 @@
 import pyautogui
 from time import sleep
 import webbrowser
-import os
+import time
 
 chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
 webbrowser.get(chrome_path).open("https://web.whatsapp.com/")
-sleep(5)
+sleep(8)
 
-pyautogui.click(384,108)
+imagem = r'C:\Users\bamar\Downloads\imagemwpp.jpg'
+
+def aguardar_imagem(caminho_imagem, timeout=30):
+    """Aguarda uma imagem aparecer na tela com timeout."""
+    inicio = time.time()
+    while True:
+        localizacao = pyautogui.locateOnScreen(caminho_imagem, confidence=0.8)
+        
+        if localizacao:
+            print(f"Imagem encontrada! O WhatsApp carregou.")
+            return localizacao
+        if time.time() - inicio > timeout:
+            print("Tempo esgotado: O WhatsApp não carregou a tempo.")
+            return None
+            
+        sleep(1) 
+
+localizacao = aguardar_imagem(imagem)
+if localizacao:
+    pyautogui.click(localizacao)
+else:
+    print("Erro ao carregar.")
+
