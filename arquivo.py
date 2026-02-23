915fe13e70ad8852fe5bba27ebd97303e2822c36
BeAmaralzin <bamaralzin@gmail.com>
2026-02-22 22:25:34 -0300

    automaão de envio de mensagens finalizada com pyautogui

--- arquivo.py
+++ arquivo.py
@@ -4,34 +4,40 @@ import webbrowser
 import time
 import pandas as pd
 
+pyautogui.FAILSAFE = True
 
 chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
 webbrowser.get(chrome_path).open("https://web.whatsapp.com/")
 
-imagem = r'C:\Users\bamar\Downloads\imagemwpp.jpg'
+print("Aguardando WhatsApp Web carregar...")
+sleep(8)  
 
-def aguardar_imagem(caminho_imagem, timeout=30):
-    """Aguarda uma imagem aparecer na tela com timeout."""
-    inicio = time.time()
-    while True:
-        localizacao = pyautogui.locateOnScreen(caminho_imagem, confidence=0.8)
+planilha = pd.read_excel(r'C:\Users\bamar\Downloads\planilha 2.xlsx',sheet_name='Planilha2')
+
+for linha in planilha.index:    
+        numero = planilha.loc[linha, 'TELEFONE']
+        sleep(0.3)
+        candidato = planilha.loc[linha, 'CANDIDATO']
+        sleep(0.3)
+        prazo_final = planilha.loc[linha, 'PRAZO FINAL']
+        sleep(0.3)
+        email = planilha.loc[linha, 'EMAIL']
+        sleep(0.3)
+
+        pyautogui.click(x=382, y=109)  
+
+        sleep(2)
+        pyautogui.write(numero)
+        sleep(2)
+        pyautogui.press('enter')
+        sleep(2) 
         
-        if localizacao:
-            print(f"Imagem encontrada! O WhatsApp carregou.")
-            return localizacao
-        if time.time() - inicio > timeout:
-            print("Tempo esgotado: O WhatsApp não carregou a tempo.")
-            return None
-            
-        sleep(1)
-
-planilha = pd.read_excel(r'C:\Users\bamar\Downloads\planilha 2.xlsx')
-
-for linha in planilha.index:
-    
-    localizacao = aguardar_imagem(imagem)
-    if localizacao:
-        pyautogui.click(localizacao)
-    else:
-        print("Erro ao carregar.")
+        mensagem = f"Olá {candidato}, tudo bem? Gostaria de informar que o prazo final para envio dos documentos é {prazo_final}. Por favor, envie os documentos para o email {email}. Obrigado!"    
+        sleep(2)
+        pyautogui.write(mensagem)
+        sleep(2)
+        pyautogui.press('enter')
+        sleep(2)  
+        
+        print(f"Mensagem enviada para {numero}\n")
 
